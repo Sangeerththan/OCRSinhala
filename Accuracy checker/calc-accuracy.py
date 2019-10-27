ff8862173a35940f89501b01186ca6e487542d4d
Manoj Nilanga <manojnilangadesilva@gmail.com>
2019-10-28 02:37:52 +0530

    accuracy checker

---
+++ Accuracy checker/calc-accuracy.py
@@ -0,0 +1,32 @@
+import codecs
+
+#f = codecs.open("testing-out.txt", encoding="utf-8")
+#f = codecs.open("tes-dic-out.txt", encoding="utf-8")
+f = codecs.open("tes-out.txt", encoding="utf-8")
+lines = f.read().splitlines()
+
+totalcharacters=0
+correctocr=0
+
+for i in range(0,len(lines)):
+	print(i)
+	split=lines[i].split("-")
+	if(len(split)!=2):
+		continue
+	ocr=split[0]
+	correct=split[1]
+	minlength=0
+	if(len(ocr)<len(correct)):
+		minlength=len(ocr)
+	else:
+		minlength=len(correct)
+		
+	totalcharacters = totalcharacters + len(correct)
+		
+	for j in range(0,minlength):
+		if(ocr[j]==correct[j]):
+			correctocr=correctocr+1
+			
+print("total cahracter count: "+str(totalcharacters))
+print("correctly ocred character counter: "+ str(correctocr))
+print("accuracy: "+str(round(correctocr*100/totalcharacters,2))+"%")
---
+++ Accuracy checker/check-tes.py
@@ -0,0 +1,39 @@
+import pytesseract
+from PIL import Image
+import glob
+import codecs
+import math
+import numpy
+import re
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
+
+mylist = [f for f in glob.glob("../generate-images-java/Output/*")]
+#mylist = [f for f in glob.glob("../generate-images-java/testout/*")]
+#mylist = [f for f in glob.glob("images/*")]
+
+g = codecs.open("tes-out.txt", "w+", encoding="utf-8")
+
+config = ('-l sin --oem 1 --psm 3')
+
+
+
+for i in range(0, len(mylist)):
+	print(i)
+	
+	text = pytesseract.image_to_string(Image.open(mylist[i]), config=config)
+	splited = mylist[i].split("/")
+	splited = splited[-1].split(".")
+	splited = splited[-2].split("\\")
+	correct = (splited[-1])
+	g.write(text+"-")
+	g.write(correct+"\n")
+	
+
+
+
+
+
+
+
+
