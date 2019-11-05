10705ee7b872b96b742413b4479e522048053843
Manoj Nilanga <manojnilangadesilva@gmail.com>
2019-11-05 09:49:53 +0530

    final2.txt

--- ocr output for dic/deletespace.py
+++ ocr output for dic/deletespace.py
@@ -2,17 +2,17 @@
 import codecs
 
 
-f = codecs.open("tes-dic-out.txt", encoding="utf-8")
+f = codecs.open("final.txt", encoding="utf-8")
 lines = f.read().splitlines()
 
 
-g = codecs.open("final.txt", "w+", encoding="utf-8")
+g = codecs.open("final2.txt", "w+", encoding="utf-8")
 
 
 for i in range(0,len(lines)):
 	print(i)
 	split=lines[i].split()
-	if(len(split)<2):
+	if(len(split)!=2):
 		continue
 	g.write(split[0]+" "+split[1]+"\n")
 	
