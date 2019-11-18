5a7395088c18da3662c61f2f64329f7f0b615f42
Manoj Nilanga <manojnilangadesilva@gmail.com>
2019-11-18 19:31:35 +0530

    edit distance accuracy checker

--- Accuracy checker/calc-accuracy.py
+++ Accuracy checker/calc-accuracy.py
@@ -1,33 +1,62 @@
 import codecs
+import editdistance
 
 #f = codecs.open("testing-out.txt", encoding="utf-8")
-#f = codecs.open("tes-dic-out.txt", encoding="utf-8")
-f = codecs.open("tes-con-out.txt", encoding="utf-8")
+f = codecs.open("tes-dic-out.txt", encoding="utf-8")
+#f = codecs.open("tes-con-out.txt", encoding="utf-8")
 #f = codecs.open("tes-out.txt", encoding="utf-8")
 lines = f.read().splitlines()
 
-totalcharacters=0
-correctocr=0
+#totalcharacters=0
+#correctocr=0
 
+totalCER = []
+totalWER = []
+totalWords = 0
+correctWords = 0
+
+	
 for i in range(0,len(lines)):
 	print(i)
+	totalWords +=1
 	split=lines[i].split("-")
 	if(len(split)!=2):
 		continue
 	ocr=split[0]
 	correct=split[1]
-	minlength=0
-	if(len(ocr)<len(correct)):
-		minlength=len(ocr)
-	else:
-		minlength=len(correct)
+
+	#removing half space at the end of the OCRed text
+	if(ocr!=""):
+		if(ord(ocr[-1])==8204):
+			ocr=ocr[:-1]
+
+	dist = editdistance.eval(ocr, correct)
+	if(dist==0):
+		correctWords += 1
+
+	currCER = dist / max(len(ocr), len(correct))
+	totalCER.append(currCER)
+	
+	#minlength=0
+	#if(len(ocr)<len(correct)):
+	#	minlength=len(ocr)
+	#else:
+	#	minlength=len(correct)
 		
-	totalcharacters = totalcharacters + len(correct)
+	#totalcharacters = totalcharacters + len(correct)
 		
-	for j in range(0,minlength):
-		if(ocr[j]==correct[j]):
-			correctocr=correctocr+1
+	#for j in range(0,minlength):
+	#	if(ocr[j]==correct[j]):
+	#		correctocr=correctocr+1
+	
+    
+	
+charErrorRate = sum(totalCER) / len(totalCER)	
+
 			
-print("total cahracter count: "+str(totalcharacters))
-print("correctly ocred character counter: "+ str(correctocr))
-print("accuracy: "+str(round(correctocr*100/totalcharacters,2))+"%")
+#print("total cahracter count: "+str(totalcharacters))
+#print("correctly ocred character counter: "+ str(correctocr))
+#print("accuracy: "+str(round(correctocr*100/totalcharacters,2))+"%")
+
+print("Charactr level accuracy: "+str(round((1-charErrorRate)*100,2))+"%")
+print("Word level accuracy: "+str(round(correctWords/totalWords*100,2))+"%")
--- Accuracy checker/test.py
+++ Accuracy checker/test.py
@@ -1,4 +1,5 @@
 import difflib
+import codecs
 
 cases=[('අනා', 'අණුක'),
        ('අණුවල', 'අණුකූල'),
@@ -6,13 +7,20 @@ cases=[('අනා', 'අණුක'),
        ('අණුව', 'අණුව'),
        ('අඳ', 'අඬ')] 
 
+g = codecs.open("testtxt.txt", "w+", encoding="utf-8")
 
 for a,b in cases:     
     print('{} => {}'.format(a,b))  
     count=0
     for i,s in enumerate(difflib.ndiff(a, b)):
-        if s[0]==' ':continue    
+        g.write("i : "+str(i)+"\n")
+        g.write("s : "+str(s)+"\n")
+        if s[0]==' ':continue   
+		#elif s[0]=='-':
+            #g.write(u'Delete "{}" from position {}'.format(s[-1],i))
+        #elif s[0]=='+':
+            #g.write(u'Add "{}" to position {}'.format(s[-1],i)) 
         count=count+1
      
-    print("count: "+str(count))
+    g.write("count: "+str(count))
 	
