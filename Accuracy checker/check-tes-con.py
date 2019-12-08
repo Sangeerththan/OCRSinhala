2aeecfa32f9e2c4bebac404446af7f4e20e6b3f1
Manoj Nilanga <manojnilangadesilva@gmail.com>
2019-12-08 13:17:04 +0530

    accuracy update for 100 words

--- Accuracy checker/check-tes-con.py
+++ Accuracy checker/check-tes-con.py
@@ -6,11 +6,12 @@ import difflib
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
 
-mylist = [f for f in glob.glob("../generate-images-java/Output/*")]
+#mylist = [f for f in glob.glob("../generate-images-java/Output/*")]
 #mylist = [f for f in glob.glob("../generate-images-java/testout/*")]
 #mylist = [f for f in glob.glob("images/*")]
+mylist = [f for f in glob.glob("../generate-images-java/testing-100-images/*")]
 
-g = codecs.open("tes-con-out.txt", "w+", encoding="utf-8")
+g = codecs.open("tes-con-out-100.txt", "w+", encoding="utf-8")
 
 config = ('-l sin --oem 1 --psm 3')
 
